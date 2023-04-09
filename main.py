394abb0c8963925e3be434c21e4dc38b87b2dc42
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-04-09 20:19:42 +0400

    Added Main functionality + Test passed ✅

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from rich.prompt import Prompt
+from rich.traceback import install
+install(show_locals = True)
 
 # path to the main picture
 main_pic_path = Prompt.ask("Input the Main Picture Path")
@@ -8,14 +10,14 @@ main_pic_path = Prompt.ask("Input the Main Picture Path")
 text = Prompt.ask("Text to be inserted over the picture")
 
 # font size and font file path
-font_size = 50
-font_path = "path/to/font.ttf"
+font_size = 150
+font_path = "C:\\Users\\hadir\\Documents\\Mulish\\Muli-Bold.ttf"
 
 # open the main picture
 main_pic = Image.open(main_pic_path)
 
 # create a new blank image with the same size as the main picture
-new_image = Image.new(mode='RGBA', size=main_pic.size, color=(0, 0, 0, 0))
+new_image = Image.new(mode='RGB', size=main_pic.size, color=(0, 0, 0, 0))
 
 # paste the main picture onto the new image
 new_image.paste(main_pic, (0, 0))
@@ -35,7 +37,7 @@ font = ImageFont.truetype(font_path, font_size)
 # get the size of the text and calculate the position
 text_width, text_height = draw.textsize(text, font=font)
 text_x = int((width - text_width) / 2)
-text_y = height - font_size - 20
+text_y = height - font_size - 150
 
 # draw the text onto the text image
 draw.text((text_x, 10), text, font=font, fill=(255, 255, 255, 255))
